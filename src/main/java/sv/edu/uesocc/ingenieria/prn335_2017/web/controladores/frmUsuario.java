8ceee827cddcb80e3e4cfe351189efd9ecf56fb6
jonathan <jongarcia7814@gmail.com>
2017-10-12 13:02:03 -0600

    Tarea Guia05

---
+++ src/main/java/sv/edu/uesocc/ingenieria/prn335_2017/web/controladores/frmUsuario.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.uesocc.ingenieria.prn335_2017.web.controladores;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.faces.bean.ViewScoped;
+import sv.edu.uesocc.ingenieria.prn335_2017.datos.acceso.UsuarioFacadeLocal;
+import sv.edu.uesocc.ingenieria.prn335_2017.datos.definiciones.Usuario;
+
+/**
+ *
+ * @author jonathanhdz
+ */
+
+@Named
+@ViewScoped
+public class frmUsuario implements Serializable {
+
+    @EJB
+    private UsuarioFacadeLocal usuarioEJB;    
+    private Usuario usuario;
+    private List<Usuario> usuarios;
+    
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+    
+    @PostConstruct
+    public void init(){
+        usuarios = usuarioEJB.findAll();
+        System.out.println(usuarios);
+    }
+
+    public UsuarioFacadeLocal getUsuarioEJB() {
+        return usuarioEJB;
+    }
+
+    public void setUsuarioEJB(UsuarioFacadeLocal usuarioEJB) {
+        this.usuarioEJB = usuarioEJB;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    
+    
+}
+
